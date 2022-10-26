074ddb818695e810715066f88f6dfb77472c4f94
bokigolic <bokigolic32@gmail.com>
2022-10-26 18:02:51 -0400

    work on style, add navbar

--- form-practicing/src/components/FormInventar.js
+++ form-practicing/src/components/FormInventar.js
@@ -2,17 +2,13 @@ import { useState } from 'react';
 import axios from 'axios'
 
 
-const FormInventar = () => {
-
-
-
+const CarInventar = () => {
   const preset = {
     make: "",
     model: "",
     color: "",
     year: ""
   }
-
   const [formState, setFormState] = useState(preset);
 
   const handleChange = (e) => {
@@ -35,73 +31,131 @@ const FormInventar = () => {
   };
 
   return (
-    <div className="form-inventar">
-      <h1>Add new car</h1>
-
-      <form>
-        <div>
-          <div>
-            <label>Make </label>
-            <input
-              type="text"
-              name="make"
-              value={formState.make}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>Model </label>
-            <input
-              type="text"
-              name="model"
-              value={formState.model}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <label>Color </label>
-            <select
-              name="color"
-              value={formState.color}
-              onChange={handleChange}
-            >
-              <option value="red" >
-                Red
-              </option>
-              <option value="blue">
-                Blue
-              </option>
-              <option value="green">
-                Green
-              </option>
-            </select>
+    <div className='for-flex'>
+      <div className="car-inventar">
+        <h1>Add new car</h1>
+        <form>
+          <div className='make-model-color-year'>
             <div>
+              <label>Make </label>
+              <input
+                type="text"
+                name="make"
+                value={formState.make}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label>Model </label>
+              <input
+                type="text"
+                name="model"
+                value={formState.model}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label>Color </label>
+              <select
+                name="color"
+                value={formState.color}
+                onChange={handleChange}
+              >
+                <option value="red" >
+                  Red
+                </option>
+                <option value="blue">
+                  Blue
+                </option>
+                <option value="green">
+                  Green
+                </option>
+                <option value="brown" >
+                  Brown
+                </option>
+                <option value="grey">
+                  Grey
+                </option>
+                <option value="white">
+                  White
+                </option>
+                <option value="yellow" >
+                  Yellow
+                </option>
+                <option value="black">
+                  Black
+                </option>
+                <option value="orange">
+                  Orange
+                </option>
+              </select>
               <div>
-                <label>Year </label>
-                <select
-                  name="year"
-                  value={formState.year}
-                  onChange={handleChange}
-                >
-                  <option value="2022">
-                    2022
-                  </option>
-                  <option value="2021">
-                    2021
-                  </option>
-                  <option value="2020">
-                    2020
-                  </option>
-                </select>
+                <div>
+                  <label>Year </label>
+                  <select
+                    name="year"
+                    value={formState.year}
+                    onChange={handleChange}
+                  >
+                    <option value="2022">
+                      2022
+                    </option>
+                    <option value="2021">
+                      2021
+                    </option>
+                    <option value="2020">
+                      2020
+                    </option>
+                    <option value="2019">
+                      2019
+                    </option>
+                    <option value="2018">
+                      2018
+                    </option>
+                    <option value="2017">
+                      2017
+                    </option>
+                    <option value="2016">
+                      2016
+                    </option>
+                    <option value="2015">
+                      2015
+                    </option>
+                    <option value="2014">
+                      2014
+                    </option>
+                    <option value="2013">
+                      2013
+                    </option>
+                    <option value="2012">
+                      2012
+                    </option>
+                    <option value="2011">
+                      2011
+                    </option>
+                    <option value="2010">
+                      2010
+                    </option>
+                    <option value="2009">
+                      2009
+                    </option>
+                    <option value="2008">
+                      2008
+                    </option>
+                    <option value="2007">
+                      2007
+                    </option>
+                  </select>
+                </div>
+                <div className='button-submit'>
+                  <button type="button" onClick={handleClickSubmit}>Submit</button>
+                </div>
               </div>
-              <button type="button" onClick={handleClickSubmit}>Submit</button>
             </div>
           </div>
-        </div>
-
-      </form >
+        </form >
+      </div>
     </div >
   );
 };
-export default FormInventar;
+export default CarInventar;
